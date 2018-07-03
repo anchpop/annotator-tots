3f1f6ce207a6144d28d54067438dc6f4258d2840
anchpop <andre@andreisawesome.com>
2018-07-02 23:54:07 -0400

    fixed path, now just need to fix automatically generated html

--- tator2/webpack_config/paths.js
+++ tator2/webpack_config/paths.js
@@ -44,7 +44,7 @@ function getServedPath(appPackageJson) {
 module.exports = {
   dotenv: resolveProjectBase('.env'),
   appBuild: resolveProjectApp('static'),
-  appPublic: resolveProjectApp('public'),
+  appPublic: resolveProjectApp(''),
   appHtml: resolveProjectApp('public/index.html'),
   appIndexJs: resolveProjectApp('src/index.jsx'),
   appPackageJson: resolveProjectBase('package.json'),
--- tator2/webpack_scripts/start.js
+++ tator2/webpack_scripts/start.js
@@ -1,5 +1,16 @@
 'use strict';
 
+// run django
+var exec = require('child_process').exec, child;
+child = exec('python manage.py runserver',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+             console.log('exec error: ' + error);
+        }
+    });
+
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
